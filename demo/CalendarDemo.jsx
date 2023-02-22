78917bbbcb7ae5d6e1f9a75042fdf1229a06db0c
马勇坤 <mayongkun@mykdeMacBook-Pro.local>
2023-02-22 11:06:53 +0800

    fix: 格式化

--- demo/CalendarDemo.jsx
+++ demo/CalendarDemo.jsx
@@ -117,189 +117,189 @@ class Demo extends React.Component {
           }
         }
       }>
-      <div className="kuma-form">
-        <div
-          className="kuma-form-field"
-          style={{
-            width: '400px',
-          }}
-        >
-          <p>基本</p>
-          <Calendar
-            showToday
-            showTime={true}
-            allowClear={false}
-            showSecond={false}
-            // locale="en-us"
-            yearSelectOffset={20}
-            yearSelectTotal={50}
-            size="middle"
-            disabledTime={() => ({
-              disabledMinutes: () => disabledMinutes,
-            })}
-            value={value}
-            onSelect={this.onSelect}
-            showDateInput
-            // renderFooter={() => {return 'aaa'}}
-          />
-        </div>
-        <div
-          className="kuma-form-field"
-          style={{
-            width: '400px',
-          }}
-        >
-          <p>日期格式</p>
-          <Calendar value={value} format="YYYY/MM/DD" onSelect={this.onSelect} showDateInput />
-        </div>
-        <div
-          className="kuma-form-field"
-          style={{
-            width: '400px',
-          }}
-        >
-          <p>选择日期</p>
-          <Calendar onSelect={this.onSelect} value={value} />
-        </div>
-        <div
-          className="kuma-form-field"
-          style={{
-            width: '400px',
-          }}
-        >
-          <p>范围</p>
-          <Calendar disabledDate={disabledDate} value={value} onSelect={this.onSelect} />
-        </div>
-        <div className="kuma-form-field">
-          <p>时间选择</p>
-          <Calendar
-            hasTrigger
-            showSecond
-            showHour
-            showTime
-            size="small"
-            timezone={8}
-            defaultOpenValue={new Date(2018, 8, 1, 3, 12)}
-            locale="en-us"
-            value={value}
-            onSelect={this.onSelect}
-          />
-        </div>
-        <div className="kuma-form-field">
-          <p>禁用</p>
-          <Calendar value={value} disabled onSelect={this.onSelect} />
-        </div>
-        <div className="kuma-form-field">
-          <p>月份</p>
-          <MonthCalendar
-            size="middle"
-            value={value}
-            disabledDate={disabledDate}
-            onSelect={this.onSelect}
-            showDateInput
-          />
-        </div>
-        <div
-          className="kuma-form-field"
-          style={{
-            width: '400px',
-          }}
-        >
-          <p>年份</p>
-          <YearCalendar
-            size="large"
-            value={value}
-            onSelect={this.onSelect}
-            disabledDate={() => {}}
-          />
-        </div>
-        <div
-          className="kuma-form-field"
-          style={{
-            width: '400px',
-          }}
-        >
-          <p>显示日期和日程</p>
-          <p>
-            Calendar 通过开放 contentRender 参数来完成日期渲染上的定制，并提供了一个默认的渲染函数
-            Calendar.util.generateContentRender(code) 来完成通用定制。
-          </p>
-          <Calendar
-            value={value}
-            onSelect={this.onSelect}
-            contentRender={Calendar.util.generateContentRender(
-              {
-                '2016-01-07': 'leave',
-                '2016-01-09': 'work',
-                '2016-01-08': 'schedule',
-              },
-              'en',
-            )}
-          />
-        </div>
-        <div className="kuma-form-field">
-          <p>直接渲染面板</p>
-          <RcCalendar {...panelOptions} className="panel-demo" />
-        </div>
-        <Button onClick={me.handleClick}>changeTime</Button>
-        <div className="kuma-form-field">
-          <p>区间日期选择</p>
-          <RangeCalendar
-            size="large"
-            value={rangeValue}
-            // locale={'en-us'}
-            quickSelectRanges={[
-              {
-                text: '19年1、2月份',
-                value: {
-                  start: '2019-01',
-                  end: '2019-02',
-                },
-              },
-              {
-                text: '18年S2',
-                value: {
-                  start: '2018-09',
-                  end: '2019-03',
-                },
-              },
-            ]}
-            onSelect={(v, formatted) => {
-              this.onRangeSelect(v, formatted);
-            }}
-          />
-        </div>
-        <div className="kuma-form-field">
-          <p>
-            大日历日期选择,跨日程，提供Calendar.util.generateScheduleContent方法，返回具体日程的相关信息
-          </p>
-          <CalendarFull
-            value={value}
-            onSelect={this.onSelect}
-            fullscreen
-            type="month"
-            locale="zh-cn"
-            format="yyyy/MM/dd"
-            scheduleRender={Calendar.fullUtil.generateScheduleContent(events, this.onScheduleClick)}
-            startHour={0}
-            endHour={24}
-            step={60}
-          />
-          <MiniWeek
-            value="2018-12-24"
-            locale="en-us"
-            events={events}
-            scheduleRender={this.miniWeekRender}
-            getPopupContainer={() => this.miniRender}
-          />
+        <div className="kuma-form">
           <div
-            className="miniweek-container"
-            ref={c => {
-              this.miniRender = c;
+            className="kuma-form-field"
+            style={{
+              width: '400px',
             }}
-          />
+          >
+            <p>基本</p>
+            <Calendar
+              showToday
+              showTime={true}
+              allowClear={false}
+              showSecond={false}
+              // locale="en-us"
+              yearSelectOffset={20}
+              yearSelectTotal={50}
+              size="middle"
+              disabledTime={() => ({
+                disabledMinutes: () => disabledMinutes,
+              })}
+              value={value}
+              onSelect={this.onSelect}
+              showDateInput
+            // renderFooter={() => {return 'aaa'}}
+            />
+          </div>
+          <div
+            className="kuma-form-field"
+            style={{
+              width: '400px',
+            }}
+          >
+            <p>日期格式</p>
+            <Calendar value={value} format="YYYY/MM/DD" onSelect={this.onSelect} showDateInput />
+          </div>
+          <div
+            className="kuma-form-field"
+            style={{
+              width: '400px',
+            }}
+          >
+            <p>选择日期</p>
+            <Calendar onSelect={this.onSelect} value={value} />
+          </div>
+          <div
+            className="kuma-form-field"
+            style={{
+              width: '400px',
+            }}
+          >
+            <p>范围</p>
+            <Calendar disabledDate={disabledDate} value={value} onSelect={this.onSelect} />
+          </div>
+          <div className="kuma-form-field">
+            <p>时间选择</p>
+            <Calendar
+              hasTrigger
+              showSecond
+              showHour
+              showTime
+              size="small"
+              timezone={8}
+              defaultOpenValue={new Date(2018, 8, 1, 3, 12)}
+              locale="en-us"
+              value={value}
+              onSelect={this.onSelect}
+            />
+          </div>
+          <div className="kuma-form-field">
+            <p>禁用</p>
+            <Calendar value={value} disabled onSelect={this.onSelect} />
+          </div>
+          <div className="kuma-form-field">
+            <p>月份</p>
+            <MonthCalendar
+              size="middle"
+              value={value}
+              disabledDate={disabledDate}
+              onSelect={this.onSelect}
+              showDateInput
+            />
+          </div>
+          <div
+            className="kuma-form-field"
+            style={{
+              width: '400px',
+            }}
+          >
+            <p>年份</p>
+            <YearCalendar
+              size="large"
+              value={value}
+              onSelect={this.onSelect}
+              disabledDate={() => { }}
+            />
+          </div>
+          <div
+            className="kuma-form-field"
+            style={{
+              width: '400px',
+            }}
+          >
+            <p>显示日期和日程</p>
+            <p>
+              Calendar 通过开放 contentRender 参数来完成日期渲染上的定制，并提供了一个默认的渲染函数
+              Calendar.util.generateContentRender(code) 来完成通用定制。
+            </p>
+            <Calendar
+              value={value}
+              onSelect={this.onSelect}
+              contentRender={Calendar.util.generateContentRender(
+                {
+                  '2016-01-07': 'leave',
+                  '2016-01-09': 'work',
+                  '2016-01-08': 'schedule',
+                },
+                'en',
+              )}
+            />
+          </div>
+          <div className="kuma-form-field">
+            <p>直接渲染面板</p>
+            <RcCalendar {...panelOptions} className="panel-demo" />
+          </div>
+          <Button onClick={me.handleClick}>changeTime</Button>
+          <div className="kuma-form-field">
+            <p>区间日期选择</p>
+            <RangeCalendar
+              size="large"
+              value={rangeValue}
+              // locale={'en-us'}
+              quickSelectRanges={[
+                {
+                  text: '19年1、2月份',
+                  value: {
+                    start: '2019-01',
+                    end: '2019-02',
+                  },
+                },
+                {
+                  text: '18年S2',
+                  value: {
+                    start: '2018-09',
+                    end: '2019-03',
+                  },
+                },
+              ]}
+              onSelect={(v, formatted) => {
+                this.onRangeSelect(v, formatted);
+              }}
+            />
+          </div>
+          <div className="kuma-form-field">
+            <p>
+              大日历日期选择,跨日程，提供Calendar.util.generateScheduleContent方法，返回具体日程的相关信息
+            </p>
+            <CalendarFull
+              value={value}
+              onSelect={this.onSelect}
+              fullscreen
+              type="month"
+              locale="zh-cn"
+              format="yyyy/MM/dd"
+              scheduleRender={Calendar.fullUtil.generateScheduleContent(events, this.onScheduleClick)}
+              startHour={0}
+              endHour={24}
+              step={60}
+            />
+            <MiniWeek
+              value="2018-12-24"
+              locale="en-us"
+              events={events}
+              scheduleRender={this.miniWeekRender}
+              getPopupContainer={() => this.miniRender}
+            />
+            <div
+              className="miniweek-container"
+              ref={c => {
+                this.miniRender = c;
+              }}
+            />
+          </div>
         </div>
-      </div>
       </UxcoreConfigProvider>
     );
   }
